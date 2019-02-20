4979f254867e253c7e70245bf1eb0071c3adf57c
Yifan Peng <yifan.peng@nih.gov>
2019-02-19 21:16:58 -0500

    .

--- tests/negbio/pipeline/test_parse.py
+++ tests/negbio/pipeline/test_parse.py
@@ -3,7 +3,7 @@ import logging
 from negbio.pipeline.parse import Bllip
 
 
-def test_parse():
+def test_Bllip():
     b = Bllip()
     t = b.parse('hello world!')
     assert str(t) == '(S1 (S (NP (NN hello) (NN world) (NN !))))'
@@ -11,4 +11,4 @@ def test_parse():
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.WARNING)
-    test_parse()
+    test_Bllip()
